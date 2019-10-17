ee114c8e3d8b99575252be8a9b4fa49b70e5aa9b
David <dimor22@gmail.com>
2019-10-16 17:03:18 -0700

    final project

---
+++ main.js
@@ -0,0 +1,13 @@
+$(document).ready(function() {
+
+  $('.open-menu-btn').click(function() {
+    $('.mobile-menu-wrap').show();
+  })
+
+  $('.close-menu-btn').click(function() {
+    $('.mobile-menu-wrap').hide();
+  })
+  $('.search-component').click(function() {
+    $('.search-box').show();
+  })
+})
